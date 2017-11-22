cc258a95ce4877fe9454c2399597f82f57c05ca2
GandhiMonroe <b.diliberto-14@student.lboro.ac.uk>
2017-11-22 14:27:14 +0000

    Login page created

---
+++ Client/src/app/login/login.component.ts
@@ -0,0 +1,25 @@
+import {Component, OnInit, Output, EventEmitter} from '@angular/core';
+import {MaterializeAction} from 'angular2-materialize';
+
+declare let Materialize: any;
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  @Output() modalActions = new EventEmitter<string|MaterializeAction>();
+  openModal() {
+    this.modalActions.emit({action: 'modal', params: ['open']});
+  }
+  closeModal() {
+    this.modalActions.emit({action: 'modal', params: ['close']});
+  }
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+}
