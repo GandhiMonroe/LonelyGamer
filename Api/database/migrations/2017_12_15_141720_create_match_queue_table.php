28f3104135e5b4b282ab3b5bec4745df3d8dac9b
GandhiMonroe <b.diliberto-14@student.lboro.ac.uk>
2017-12-15 14:24:37 +0000

    Match Queue architecture

---
+++ Api/database/migrations/2017_12_15_141720_create_match_queue_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMatchQueueTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('match_queue', function (Blueprint $table) {
+            $table->integer('userID');
+            $table->integer('gameID');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('match_queue');
+    }
+}
